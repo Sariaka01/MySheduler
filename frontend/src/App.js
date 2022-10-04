3eff9e77d877498d8999502a336aba1d5ab1c1c6
Fanirintsoa Mathieu <fanirintsoaandrianandraina@gmail.com>
2022-10-04 10:32:25 +0300

    Initialization

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React  from "react";
+import './App.css';
+
+
+function App(){
+ 
+  return (
+    
+    <div className="App">
+      <h1>Mendrika</h1>
+     
+    </div> 
+  
+  );
+
+}
+export default App;
+  

7c39c89da94f65b2a30d367c234bb7dd0b169c5a
Arindranto <safidy.herinirina@gmail.com>
2022-10-07 21:10:23 +0300

    Added variables and types modules

---
+++ front-end/servercode/globals/types.ts
@@ -0,0 +1,3 @@
+export interface GeneralObject {
+    [name: string]: any
+}
---
+++ front-end/servercode/globals/variable.ts
@@ -0,0 +1,3 @@
+export const TOKEN_SECRET = process.env.TOKEN_SECRET || 'MySchedulerToken'
+export const PORT = process.env.PORT || 3001
+export const HOSTNAME= process.env.HOSTNAME || 'localhost'

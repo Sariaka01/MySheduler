642b77dae972db661c129d1f3265bb1fddf062f7
Arindranto <safidy.herinirina@gmail.com>
2022-10-07 21:11:35 +0300

    Updated dependencies

--- front-end/servercode/main.ts
+++ front-end/servercode/main.ts
@@ -1,13 +1,16 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import userRouter from './routes/userRoute'
+import cors from 'cors'
+import { HOSTNAME, PORT } from './globals/variable'
 
 dotenv.config() // Config environment variables
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use('/user', userRouter)
 
 app.listen(process.env.PORT || 3001, () => {
-    console.log(`listening at http://${process.env.HOSTNAME || "localhost"}:${process.env.PORT}`)
+    console.log(`listening at http://${HOSTNAME || "localhost"}:${PORT}`)
 })

56cf9abe16ed650767559dec5305f661c29945e0
Arindranto <safidy.herinirina@gmail.com>
2022-10-08 15:41:52 +0300

    updated test datas

--- front-end/src/test/list.js
+++ front-end/src/test/list.js
@@ -3,8 +3,8 @@ const LIST= [
         "id": 0,
         "name": "Tache 1",
         "creator": "Mendika",
-        "Start": "12-12-2022 09:30",
-        "End": "12-12-2022 17:00",
+        "start": "12-12-2022 09:30",
+        "end": "12-12-2022 17:00",
         "description": "BOnjour le monde, voici task manager",
         "participants": ["Mendrika", "Faniry", "Safidy", "Sariaka", "Tsiory", "Kevin"]
     },
@@ -12,8 +12,26 @@ const LIST= [
         "id": 1,
         "name": "Tache 2",
         "creator": "Faniry",
-        "Start": "12-12-2022 09:30",
-        "End": "12-12-2022 17:00",
+        "start": "12-12-2022 09:30",
+        "end": "12-12-2022 17:00",
+        "description": "BOnjour le monde, voici task manager",
+        "participants": ["Sariaka", "Tsiory", "Kevin"]
+    },
+    {
+        "id": 2,
+        "name": "Tache 2",
+        "creator": "Faniry",
+        "start": "12-12-2022 09:30",
+        "end": "12-12-2022 17:00",
+        "description": "BOnjour le monde, voici task manager",
+        "participants": ["Sariaka", "Tsiory", "Kevin"]
+    },
+    {
+        "id": 3,
+        "name": "Tache 2",
+        "creator": "Faniry",
+        "start": "12-12-2022 09:30",
+        "end": "12-12-2022 17:00",
         "description": "BOnjour le monde, voici task manager",
         "participants": ["Sariaka", "Tsiory", "Kevin"]
     }

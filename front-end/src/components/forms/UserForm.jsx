fac0ee038bc47b46a4c1a6cc157311b6bfeae4ef
Fanirintsoa Mathieu <fanirintsoaandrianandraina@gmail.com>
2022-10-12 08:58:41 +0300

    Form Style

--- front-end/src/components/forms/UserForm.jsx
+++ front-end/src/components/forms/UserForm.jsx
@@ -2,17 +2,21 @@ import React, { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import Registration from './Registration'
 import Login from './Login'
-import './user-form.css'
+// import './user-form.css'
+import '../../form.css'
 
 function UserForm() {
     const [isLogin, setIsLogin] = useState(true)
     return (
-        <div>
-            <div>
+        <div className="container-login">
+            
+            <div className="wrap-login">
+            <div className="choice">
                 <button onClick = {() => setIsLogin(true)}>LOGIN</button>
                 <button onClick = {() => setIsLogin(false)}>REGISTER</button>
             </div>
-            {isLogin && <Login /> || <Registration />}
+                {isLogin && <Login /> || <Registration />}
+            </div>
         </div>
     )
 }
